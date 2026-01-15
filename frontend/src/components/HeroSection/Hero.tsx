75e6cd72adbe9d6985154a5032e1fe8c398a5371
the25thjin <mannr075@gmail.com>
2026-01-15 23:47:34 +0530

    added responsiveness

--- frontend/src/components/HeroSection/Hero.tsx
+++ frontend/src/components/HeroSection/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className="flex flex-col  items-center justify-center ">
       <MainHeading/>
-      <div className="video-section w-[90%] h-[570px] mt-4 mb-10  ">
+      <div className="video-section w-[90%] mb-3 mt-5 sm:mb-10  ">
           <LoomEmbed  src="https://www.loom.com/embed/ca59f8d9ceca466e9f7e53162b1fd493?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true" />
       </div>
     </div>
